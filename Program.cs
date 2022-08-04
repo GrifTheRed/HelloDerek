7b2a0ed2aad65b7700ea7f188f3e8d3916475c27
Derek Griffin <griffinderek6@gmail.com>
2022-08-04 09:24:44 -0600

    added more lines

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Derek!");
+Console.WriteLine("Do you think the Raiders are going to win the Super Bowl this year?");
+Console.WriteLine("They best or I might be done with football");
