f49b8be312bbe1f23ab03a67b94db88222657716
Derek Griffin <griffinderek6@gmail.com>
2022-08-04 09:31:04 -0600

    added more lines

--- Program.cs
+++ Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Derek!");
 Console.WriteLine("Do you think the Raiders are going to win the Super Bowl this year?");
 Console.WriteLine("They best or I might be done with football");
+Console.WriteLine("Does the VA provide good care to their patients?");
+Console.WriteLine("No, no they do not. You can't run healthcare like a post office");
